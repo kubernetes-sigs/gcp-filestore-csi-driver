f0e29194d658f3658ed906bb17114620e2bed70d
Michelle Au <msau@google.com>
2018-07-16 17:18:15 -0700

    Add cluster-admin binding for GKE

--- deploy/kubernetes/cluster_setup.sh
+++ deploy/kubernetes/cluster_setup.sh
@@ -8,6 +8,11 @@ mydir="$(dirname $0)"
 
 source "$mydir/../common.sh"
 
+# GKE requires this extra cluster-admin rolebinding in order to create clusterroles
+if ! kubectl get clusterrolebinding cluster-admin binding; then
+  kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user $(gcloud config get-value account)
+fi
+
 kubectl apply -f "$mydir/manifests/setup_cluster.yaml"
 
 if ! kubectl get secret gcp-filestore-csi-driver-sa --namespace=$GCFS_NS; then
