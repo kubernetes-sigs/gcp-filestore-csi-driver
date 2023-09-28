2c480e1051eb1923c38b9b8557ab3aa403597206
Harry Sadoyan <hsadoyan@google.com>
2023-09-28 20:11:59 +0000

    Make pkgdir match k8s_e2e dir

--- test/run_e2e_local.sh
+++ test/run_e2e_local.sh
@@ -3,6 +3,6 @@
 set -o nounset
 set -o errexit
 
-readonly PKGDIR=${GOPATH}/src/sigs.k8s.io/gcp-filestore-csi-driver
+readonly PKGDIR=${GOPATH}/src/github.com/kubernetes-sigs/gcp-filestore-csi-driver
 
 ginkgo -v -trace "${PKGDIR}/test/e2e/tests" --logtostderr -- --project ${PROJECT} --service-account ${GCFS_IAM_NAME}
