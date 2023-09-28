7b661c053246b3c4fee86b6a359a39c62f533779
Harry Sadoyan <hsadoyan@google.com>
2023-09-28 20:11:59 +0000

    Make pkgdir match k8s_e2e dir

--- deploy/kubernetes/install_kustomize.sh
+++ deploy/kubernetes/install_kustomize.sh
@@ -7,7 +7,7 @@
 set -o nounset
 set -o errexit
 
-readonly INSTALL_DIR="${GOPATH}/src/sigs.k8s.io/gcp-filestore-csi-driver/bin"
+readonly INSTALL_DIR="${GOPATH}/src/github.com/kubernetes-sigs/gcp-filestore-csi-driver/bin"
 readonly KUSTOMIZE_PATH="${INSTALL_DIR}/kustomize"
 
 if [ ! -f "${INSTALL_DIR}" ]; then
