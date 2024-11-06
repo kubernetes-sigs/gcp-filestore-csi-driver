2095c13b47644df75e057a0f4bc5e5a457a42f26
Siyan Shen <shensiyan@google.com>
2024-11-05 16:54:09 -0800

    Revert "[release-1.6] Lock release unit test"

--- pkg/releaselock/rpc.go
+++ pkg/releaselock/rpc.go
@@ -41,8 +41,6 @@ const (
 	notifyCloseChannelSize = 1
 )
 
-type FileStoreRPCClient struct{}
-
 type releaseLockResponse struct {
 	status releaseLockStatus
 }
@@ -71,7 +69,7 @@ func RegisterLockReleaseProcedure() error {
 // ReleaseLock calls the Filestore server to remove all advisory locks for a given GKE node IP.
 // hostIP is the internal IP address of the Filestore instance.
 // clientIP is the internal IP address of the GKE node.
-func (c *FileStoreRPCClient) ReleaseLock(hostIP, clientIP string) error {
+func ReleaseLock(hostIP, clientIP string) error {
 	// Check for valid IPV4 address.
 	if net.ParseIP(hostIP) == nil {
 		return fmt.Errorf("invalid Filestore IP address %s", hostIP)
